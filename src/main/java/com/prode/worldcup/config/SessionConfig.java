dd0832042990548bbc1fdc8d542a31e95dfe5dc9
Mariano Amigo <marianoamigo93@gmail.com>
2026-06-14 01:53:07 -0300

    fix sessions

--- src/main/java/com/prode/worldcup/config/SessionConfig.java
+++ src/main/java/com/prode/worldcup/config/SessionConfig.java
@@ -2,10 +2,12 @@ package com.prode.worldcup.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 import org.springframework.session.web.http.CookieSerializer;
 import org.springframework.session.web.http.DefaultCookieSerializer;
 
 @Configuration
+@EnableJdbcHttpSession(maxInactiveIntervalInSeconds = 30 * 24 * 60 * 60)
 public class SessionConfig {
 
     @Bean
